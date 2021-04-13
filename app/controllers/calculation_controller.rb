193174b53144f4c421ce939d717dec8043a778f7
yyyyuyatakeuchi <yuzaizhunei@gmail.com>
2021-04-13 15:03:01 +0900

    addRatings

--- app/controllers/calculation_controller.rb
+++ app/controllers/calculation_controller.rb
@@ -1,21 +1,27 @@
 class CalculationController < ApplicationController
   def top
     @matchs = params[:matchs].to_i
+    @rates = params[:rates].to_i
   end
 
   def calculate
     @score_total = 0
-    @cal = "( "
+    @score_record = []
+    @cal_formula = "( "
     @matchs = params[:matchs].to_i
+    @rates = params[:rates].to_i
     @matchs.times do |n|
       score = params[:number]["#{n+1}"].to_i
       @score_total += score
+      @score_record << score
       score = "(#{score})" if score < 0
-      @cal += (score.to_s + "+" )
+      @cal_formula += (score.to_s + "+" )
     end
     @result_total = @score_total - 250 * @matchs
-    @cal.chop!
-    @cal += " ) ー ( 250×#{@matchs} )"
+    @result = @result_total * 10 * @rates
+    @cal_formula.chop!
+    @cal_formula += " ) ー ( 250×#{@matchs} )"
+    #@cal += " = ( #{@score_total} ) - #{250 * @matchs}"
     render :top
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'calculation#top'
+  get 'calculation/top'
   get 'calculation/calculate'
+  post 'calculation/calculate'
 end

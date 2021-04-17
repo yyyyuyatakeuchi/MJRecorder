a249870b2d20f908e9173a83f4e982997cae3022
yyyyuyatakeuchi <yuzaizhunei@gmail.com>
2021-04-17 12:29:01 +0900

    基準点などのsettings要素ついか

--- app/controllers/calculation_controller.rb
+++ app/controllers/calculation_controller.rb
@@ -1,7 +1,8 @@
 class CalculationController < ApplicationController
   def top
-    @rates = params[:rates].to_i
+    @rates = params[:rates].to_f
     @matchs = params[:matchs].to_i
+    @basePoints = params[:basePoints].to_i
   end
 
   def matchCount
@@ -14,7 +15,8 @@ class CalculationController < ApplicationController
     @score_record = []
     @cal_formula = "( "
     @matchs = params[:matchs].to_i
-    @rates = params[:rates].to_i
+    @rates = params[:rates].to_f
+    @basePoints = params[:basePoints].to_i
     @matchs.times do |n|
       score = params[:number]["#{n+1}"].to_i
       @score_total += score
@@ -22,10 +24,11 @@ class CalculationController < ApplicationController
       score = "(#{score})" if score < 0
       @cal_formula += (score.to_s + "+" )
     end
-    @result_total = @score_total - 250 * @matchs
-    @result = @result_total * 10 * @rates
+    @difference = 25000 / @basePoints
+    @result_total = @score_total - @basePoints * @matchs
+    @result = @result_total * @difference * @rates
     @cal_formula.chop!
-    @cal_formula += " ) ー ( 250×#{@matchs} )"
+    @cal_formula += " ) ー ( #{@basePoints}×#{@matchs} )"
     #@cal += " = ( #{@score_total} ) - #{250 * @matchs}"
     render :top
   end
